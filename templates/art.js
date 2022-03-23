8de344038e705afd06b871e7af7aa560159a0139
Yi-Jung0430 <yijunglin79013@gmail.com>
2022-03-23 17:28:16 +0800

    測試 jquery 版本

--- templates/art.js
+++ templates/art.js
@@ -26,4 +26,25 @@ $(document).ready(function() {
     });
     //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
   
+
+
+    // 輪播區
+            
+    $('.active-slider').owlCarousel({
+      items: 1,
+      loop: true,
+      nav: true,
+      //animation
+      // animateOut: 'fadeOut',
+      slideSpeed : 200,
+      autoplay: true,
+      //icon for nav
+      navText: ['<h1> < </h1>', '<h1> > </h1>']
+    });
+
+  
   });
+
+  function myFunc(){
+    alert("You Clicked me");
+ }
